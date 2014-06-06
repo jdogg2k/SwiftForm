662582fd03c7918dcaa90847cb1055b6fc434f1f
jdogg2k <jordanrose@kemtahmac.local>
2014-06-06 11:59:07 -0400

    through part5

---
+++ SwiftForm/APIController.swift
@@ -0,0 +1,55 @@
+//
+//  APIController.swift
+//  SwiftForm
+//
+//  Created by Jordan Rose on 6/6/14.
+//  Copyright (c) 2014 Jordan Rose. All rights reserved.
+//
+
+import UIKit
+
+protocol APIControllerProtocol {
+    func didRecieveAPIResults(results: NSDictionary)
+}
+
+class APIController: NSObject {
+    
+    var delegate: APIControllerProtocol?
+    
+    var data: NSMutableData = NSMutableData()
+    
+    func searchItunesFor(searchTerm: String) {
+        
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        var itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        // Now escape anything else that isn't URL-friendly
+        var escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+        var urlPath = "https://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+        var url: NSURL = NSURL(string: urlPath)
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)
+        
+        println("Search iTunes API at URL \(url)")
+        
+        connection.start()
+    }
+    
+    func connection(didReceiveResponse: NSURLConnection!, didReceiveResponse response: NSURLResponse!) {
+        // Recieved a new request, clear out the data object
+        self.data = NSMutableData()
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
+        // Append the recieved chunk of data to our data object
+        self.data.appendData(data)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        // Request complete, self.data should now hold the resulting info
+        // Convert the retrieved data in to an object through JSON deserialization
+        var err: NSError
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+        delegate?.didRecieveAPIResults(jsonResult)
+    }
+}
